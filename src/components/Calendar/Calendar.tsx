b34165f79e12ae4d7352e69392eaebe9d85d74f4
DevCeur <dev.carlos.umana@gmail.com>
2021-03-26 15:06:34 -0500

    [feat]: Create Cells component functionality

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -9,29 +9,28 @@ import Cells from "components/Cells";
 const Calendar = () => {
   const [dates, setDates] = useState({
     currentMonth: new Date(),
-    selectedMonth: new Date(),
   });
 
   const handlePrevMonth = (e: React.MouseEvent) => {
     e.preventDefault();
-    setDates({ ...dates, currentMonth: addMonths(dates.currentMonth, 1) });
+    setDates({ ...dates, currentMonth: subMonths(dates.currentMonth, 1) });
   };
 
   const handleNextMonth = (e: React.MouseEvent) => {
     e.preventDefault();
-    setDates({ ...dates, currentMonth: subMonths(dates.currentMonth, 1) });
+    setDates({ ...dates, currentMonth: addMonths(dates.currentMonth, 1) });
   };
 
   return (
-    <Center w="100vw" h="100vh">
-      <Flex direction="column" maxW={700} w="85%">
+    <Center w="100vw">
+      <Flex direction="column" maxW={700} w="85%" mt={24}>
         <Header
           currentMonth={dates.currentMonth}
           onPrevMonth={handlePrevMonth}
           onNextMonth={handleNextMonth}
         />
         <Days currentMonth={dates.currentMonth} />
-        <Cells />
+        <Cells currentMonth={dates.currentMonth} />
       </Flex>
     </Center>
   );
--- src/components/Cells/Cells.tsx
+++ src/components/Cells/Cells.tsx
@@ -1,11 +1,125 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  endOfWeek,
+  format,
+  startOfMonth,
+  startOfWeek,
+  endOfMonth,
+  isToday,
+  isBefore,
+  addDays,
+  isSameDay,
+} from "date-fns";
+import { Badge, Box, Center, Flex, Text } from "@chakra-ui/react";
+import { RiCheckboxBlankCircleFill } from "react-icons/ri";
 
-const Cells = () => {
-  return (
-    <Flex>
-      <Text>Cells</Text>
-    </Flex>
-  );
+interface CellsProps {
+  currentMonth: Date;
+}
+
+const Cells = ({ currentMonth }: CellsProps) => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const dateFormat = "d";
+  const rows = [];
+
+  const monthStart = startOfMonth(currentMonth);
+  const monthEnd = endOfMonth(currentMonth);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  let days = [];
+  let day = startDate;
+  let formattedDate = "";
+
+  const handleClickDay = (selectedDate: Date) => {
+    setSelectedDate(selectedDate);
+  };
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = format(day, dateFormat);
+
+      const cloneDay = day;
+
+      days.push(
+        <Box
+          key={day.toString()}
+          w="100%"
+          position="relative"
+          p={8}
+          borderBottom="1px"
+          borderLeft="1px"
+          borderColor="gray.100"
+          _last={{
+            borderBottom: "1px",
+            borderRight: "1px",
+            borderColor: "gray.100",
+          }}
+          bgColor={isBefore(day, new Date()) ? "gray.50" : "transparent"}
+          cursor={isBefore(day, new Date()) ? "default" : "pointer"}
+          pointerEvents={isBefore(day, new Date()) ? "none" : "all"}
+          _hover={{
+            bgColor: isBefore(day, new Date()) ? "gray.50" : "teal.50",
+          }}
+          onClick={() => handleClickDay(cloneDay)}
+        >
+          {isToday(day) && (
+            <Center>
+              <Badge
+                fontSize="8px"
+                position="absolute"
+                top="0"
+                mx="auto"
+                colorScheme="teal"
+              >
+                Today
+              </Badge>
+            </Center>
+          )}
+          {isSameDay(day, selectedDate) && (
+            <Box
+              color="blue.500"
+              fontSize="8px"
+              position="absolute"
+              top="0"
+              right="0"
+              p={2}
+            >
+              <RiCheckboxBlankCircleFill />
+            </Box>
+          )}
+          <Text
+            fontSize="sm"
+            fontWeight={isToday(day) ? "bold" : "regular"}
+            color="gray.500"
+            textAlign="center"
+            _hover={{
+              color: isBefore(day, new Date()) ? "gray.500" : "teal.500",
+            }}
+          >
+            {formattedDate}
+          </Text>
+        </Box>
+      );
+
+      day = addDays(day, 1);
+    }
+
+    rows.push(
+      <Flex
+        key={day.toString()}
+        w="100%"
+        justify="space-between"
+        _first={{ borderTop: "1px", borderTopColor: "gray.100" }}
+      >
+        {days}
+      </Flex>
+    );
+    days = [];
+  }
+
+  return <Box w="100%">{rows}</Box>;
 };
 
 export default Cells;
